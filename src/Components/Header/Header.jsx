8ba14e4c9aeeaac6aaa7f9f19db9200147e36c2f
triveni <trivenilande95@gmail.com>
2024-06-24 16:29:01 +0530

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favorite food here!</h2>
+            <p>Your go-to food delivery app for delicious meals delivered straight to your door. Whether you're craving comfort food, healthy eats, or a gourmet experience, our extensive menu has something for everyone.</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
